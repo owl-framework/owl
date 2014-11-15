5ea2a4cb4c94bf29acc623cc6216ece72ef52162
Patsura Dmitry <zaets28rus@gmail.com>
2014-11-15 14:31:48 +0900

    Application test case

---
+++ tests/Owl/Tests/ApplicationTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Patsura Dmitry <zaets28rus@gmail.com>
+ */
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $this->assertTrue(true);
+    }
+} 
