e30f154a0902e9b35490c118471bf4658b1374b0
Patsura Dmitry <zaets28rus@gmail.com>
2014-11-15 14:32:11 +0900

    Remove unused use for RequestTest

--- tests/Owl/Tests/Http/RequestTest.php
+++ tests/Owl/Tests/Http/RequestTest.php
@@ -3,9 +3,6 @@
  * @author Patsura Dmitry <zaets28rus@gmail.com>
  */
 
-use Lynx\Annotation\RegexDocParser;
-use Lynx\Annotation\ReflectionClassParser;
-
 class RequestTest extends \PHPUnit_Framework_TestCase
 {
     public function testParseAnnotation()
