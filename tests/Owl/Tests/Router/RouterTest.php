68c633961f989d450c6e6bfbd670f3efcd53acf0
Дмитрий Пацура <talk@dmtry.me>
2014-12-25 17:25:23 +0900

    Router TestCase added more tests for static router

--- tests/Owl/Tests/Router/RouterTest.php
+++ tests/Owl/Tests/Router/RouterTest.php
@@ -9,9 +9,18 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     {
         $router = new \Owl\Router\Router();
         $router->add("/", ['name' => 'default', 'action' => 'index', 'controller' => 'index', 'module' => 'index']);
-        $router->add("/user/1/", ['name' => 'user-view-static', 'action' => 'view', 'controller' => 'user', 'module' => 'user']);
+        $router->add("/user/1/", ['name' => 'user-view-static-1', 'action' => 'view', 'controller' => 'user', 'module' => 'user']);
+        $router->add("/user/100/", ['name' => 'user-view-static-100', 'action' => 'view', 'controller' => 'user', 'module' => 'user']);
 
         $this->assertInstanceOf('Owl\Router\Route', $result = $router->match("/"));
         $this->assertSame("default", $result->parameters["name"]);
+
+        $this->assertInstanceOf('Owl\Router\Route', $result = $router->match("/user/1/"));
+        $this->assertSame("user-view-static-1", $result->parameters["name"]);
+
+        $this->assertInstanceOf('Owl\Router\Route', $result = $router->match("/user/100/"));
+        $this->assertSame("user-view-static-100", $result->parameters["name"]);
+
+        $this->assertFalse($router->match('/not-found-route'));
     }
 } 
