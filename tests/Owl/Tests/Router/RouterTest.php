c15a055cd54b8825e55e10e0667a4490923edef8
Дмитрий Пацура <talk@dmtry.me>
2014-12-25 17:21:58 +0900

    Added simple test for Router

--- tests/Owl/Tests/Router/RouterTest.php
+++ tests/Owl/Tests/Router/RouterTest.php
@@ -5,8 +5,12 @@
 
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testParseAnnotation()
+    public function testGetFixtures()
     {
-        $this->assertTrue(true);
+        $router = new \Owl\Router\Router();
+        $router->add("/", ['name' => 'default', 'action' => 'index', 'controller' => 'index', 'module' => 'index']);
+        $router->add("/user/1/", ['name' => 'user-view-static', 'action' => 'view', 'controller' => 'user', 'module' => 'user']);
+
+        $this->assertInstanceOf('Owl\Router\Route', $router->match("/"));
     }
 } 
