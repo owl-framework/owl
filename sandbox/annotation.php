ee47d5a55cbbe636e816b3e45611590da64aa1a9
Дмитрий Пацура <talk@dmtry.me>
2015-01-04 17:17:22 +0900

    Sandbox with Annotations

--- sandbox/annotation.php
+++ sandbox/annotation.php
@@ -3,3 +3,15 @@
 include_once __DIR__ . '/../examples/rest-app/app/modules/api/controllers/UserController.php';
 
 $refClass = new ReflectionClass('UserController');
+$parse = \Owl\Annotations\Reader::parse($refClass->getDocComment());
+var_dump($parse);
+
+foreach ($parse as $expr) {
+
+}
+
+
+foreach ($refClass->getMethods() as $method) {
+    $parse = \Owl\Annotations\Reader::parse($method->getDocComment());
+    var_dump($parse);
+}
