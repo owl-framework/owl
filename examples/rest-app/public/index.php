e16926936e4cd645bf5a99e3d3b1f63844d1f855
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 21:22:44 +0900

    Added 5 callbacks for main events of App - to test performance

--- examples/rest-app/public/index.php
+++ examples/rest-app/public/index.php
@@ -21,6 +21,35 @@ $router->add('/users/:id', ['module' => 'Api', 'controller' => 'User', 'action' 
 
 $serviceManager->setService('router', $router);
 
+$eventManager = new \Owl\Event\Manager();
+$eventManager->listen('dispatch:afterInitialize', function($app) {
+    var_dump('dispatch:afterInitialize');
+});
+
+$eventManager->listen('dispatch:afterAction', function($app) {
+    var_dump('dispatch:afterAction');
+});
+
+$eventManager->listen('dispatch:afterAction', function($app) {
+    var_dump('dispatch:afterAction');
+});
+
+$eventManager->listen('dispatch:beforeExecuteRoute', function($app) {
+    var_dump('dispatch:beforeExecuteRoute');
+});
+
+$eventManager->listen('dispatch:beforeExecuteRoute', function($app) {
+    var_dump('dispatch:beforeExecuteRoute');
+});
+
+$eventManager->listen('dispatch:afterExecuteRoute', function($app) {
+    var_dump('dispatch:afterExecuteRoute');
+});
+
+$eventManager->listen('app:afterHandle', function($app) {
+    var_dump('app:afterHandle');
+});
+
 $application = new Application($serviceManager);
 $response = $application->handle(Request::createFromGlobals(), new \Owl\Http\Response\Json());
 $response->send();
