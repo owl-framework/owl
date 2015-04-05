e10dfdebc747b155b1635fbe1e2c9a632cf55f8f
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 17:11:45 +0900

    Added more tests for Http\Request

--- tests/Owl/Tests/Http/RequestTest.php
+++ tests/Owl/Tests/Http/RequestTest.php
@@ -9,11 +9,22 @@ class RequestTest extends \PHPUnit_Framework_TestCase
 {
     public function testRequest()
     {
-        $_SERVER['REQUEST_URI'] = '/';
+        unset($_SERVER['REQUEST_URI']);
+        $request = Request::createFromGlobals();
 
+        $this->assertEquals('/', $request->getUri()); // By default it's /
+        $this->assertEquals(Request::GET, $request->getMethod());
+
+        $_SERVER['REQUEST_URI'] = '/';
         $request = Request::createFromGlobals();
 
         $this->assertEquals('/', $request->getUri());
+        $this->assertEquals(Request::GET, $request->getMethod());
+
+        $_SERVER['REQUEST_URI'] = '/test';
+        $request = Request::createFromGlobals();
+
+        $this->assertEquals('/test', $request->getUri());
         $this->assertEquals(Request::GET, $request->getMethod());
     }
 } 
