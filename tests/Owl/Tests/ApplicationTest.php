1c4387e834ab41c4fdfd5cd40bf4a120f2889e1b
Дмитрий Пацура <talk@dmtry.me>
2015-04-05 17:08:12 +0900

    Added tests for Application

--- tests/Owl/Tests/ApplicationTest.php
+++ tests/Owl/Tests/ApplicationTest.php
@@ -3,10 +3,38 @@
  * @author Patsura Dmitry <zaets28rus@gmail.com>
  */
 
+use Owl\Application;
+use Owl\Http\Request;
+use Owl\Router\Router;
+
 class ApplicationTest extends \PHPUnit_Framework_TestCase
 {
+    protected function createSmallApp()
+    {
+        $serviceManager = new Owl\Service\Manager();
+
+        $router = new Router();
+        $router->add('/', ['module' => 'Api', 'controller' => 'Index', 'action' => 'index']);
+
+        $serviceManager->setService('router', $router);
+
+        return new Application($serviceManager);
+    }
+
+    public function testCreateSmallApp()
+    {
+        $application = $this->createSmallApp();
+
+        $this->assertInstanceOf('Owl\Application', $application);
+        $this->assertInstanceOf('Owl\Service\Manager', $application->getDi());
+        $this->assertSame(Application::ENV_PRODUCTION, $application->getEnv());
+    }
+
     public function testHandle()
     {
-        $this->assertTrue(true);
+        $application = $this->createSmallApp();
+        $response = $application->handle(Request::createFromGlobals(), new \Owl\Http\Response\Json());
+
+        $this->assertInstanceOf('Owl\Http\ResponseInterface', $response);
     }
 } 
