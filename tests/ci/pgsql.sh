018cc9616c0926dfbb13c934cce207af71f7e0de
Dmitry Patsura <talk@dmtry.me>
2015-05-05 00:40:19 +0900

    Fix typo

--- tests/ci/pgsql.sh
+++ tests/ci/pgsql.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 psql -U postgres -c 'create database owl_test;'
-psql -U postgres -d owl_test -f tests/schemas/pqsql/dump.sql
+psql -U postgres -d owl_test -f tests/schemas/pgsql/dump.sql
