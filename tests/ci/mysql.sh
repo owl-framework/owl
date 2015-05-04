c36032ff21560fe9df56cd0136877c5367141309
Dmitry Patsura <talk@dmtry.me>
2015-05-04 22:28:13 +0900

    Fix typo

--- tests/ci/mysql.sh
+++ tests/ci/mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mysql -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
-mysql -uroot lynx_test < ./tests/schemas/mysql/dump.sql
+mysql -uroot owl_test < ./tests/schemas/mysql/dump.sql
--- tests/ci/pqsql.sh
+++ tests/ci/pqsql.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 psql -U postgres -c 'create database owl_test;'
-psql -U postgres -d lynx_test -f tests/schemas/pqsql/dump.sql
+psql -U postgres -d owl_test -f tests/schemas/pqsql/dump.sql
