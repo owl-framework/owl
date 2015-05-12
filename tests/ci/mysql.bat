d8f14cf3cf55ecf77e49be541f972f90a8d0c157
Dmitry Patsura <talk@dmtry.me>
2015-05-13 02:19:26 +0900

    Try to fix

--- tests/ci/mysql.bat
+++ tests/ci/mysql.bat
@@ -1,2 +1,2 @@
-mysql -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
-mysql -uroot owl_test < ./tests/schemas/mysql/dump.sql
+mysql.exe -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
+mysql.exe -uroot owl_test < ./tests/schemas/mysql/dump.sql
