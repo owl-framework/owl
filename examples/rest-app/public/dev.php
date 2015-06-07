bb77610c6b4b12335227af8d22f438ea80307193
Dmitry Patsura <talk@dmtry.me>
2015-06-07 19:42:19 +0900

    [rest-app] Split to dev and production files

--- examples/rest-app/public/dev.php
+++ examples/rest-app/public/dev.php
@@ -14,6 +14,13 @@ ini_set('display_errors', 1);
 include_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../app/Application.php';
 
-$application = new \RestApp\Application(new Owl\Service\Manager());
+$eventManager = new \Owl\Event\Manager();
+
+$profiler = new Owl\Debug\Profiler();
+$profiler->listen($eventManager);
+
+$application = new \RestApp\Application(new Owl\Service\Manager(), $eventManager);
 $response = $application->bootstrap()->handle(Request::createFromGlobals(), new \Owl\Http\Response\Json());
+
+$response = $profiler->setContent($response);
 $response->send();
