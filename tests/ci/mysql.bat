75f42219860c43a73a54bbe56d9997ff350599ba
Dmitry Patsura <zaets28rus@gmail.com>
2015-08-04 22:16:53 +0800

    Try to fix mysql on AppVeyor

--- tests/ci/mysql.bat
+++ tests/ci/mysql.bat
@@ -1,2 +1,2 @@
-mysql -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
-mysql -uroot owl_test < ./tests/schemas/mysql/dump.sql
+C:\tools\mysql\bin\mysql.exe -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
+C:\tools\mysql\bin\mysql.exe -uroot owl_test < ./tests/schemas/mysql/dump.sql
