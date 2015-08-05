04e477ac242aa3bbcb55c1ffe930af192c325b63
Dmitry Patsura <talk@dmtry.me>
2015-08-06 01:36:10 +0900

    AppVeyor - fix mysql load

--- tests/ci/mysql.bat
+++ tests/ci/mysql.bat
@@ -1,2 +1,2 @@
-C:\tools\mysql\bin\mysql.exe -uroot -e 'create database owl_test charset=utf8 collate=utf8_unicode_ci;'
-C:\tools\mysql\bin\mysql.exe -uroot owl_test < ./tests/schemas/mysql/dump.sql
+mysqladmin --host=localhost --user=root create owl_test
+mysql -uroot owl_test < ./tests/schemas/mysql/dump.sql
