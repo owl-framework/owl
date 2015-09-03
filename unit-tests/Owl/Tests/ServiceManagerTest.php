000febdfd33458ea23ebbd55405c7dc0c2401323
Dmitry Patsura <talk@dmtry.me>
2015-09-03 19:05:33 +0900

    More tests on Di

--- unit-tests/Owl/Tests/ServiceManagerTest.php
+++ unit-tests/Owl/Tests/ServiceManagerTest.php
@@ -12,7 +12,21 @@ class ServiceManagerTest extends \PHPUnit_Framework_TestCase
         $serviceManager = new \Owl\Di\Container();
         $this->assertFalse($serviceManager->has('test'));
 
-        $serviceManager->setService('test', new \Owl\Router\Router());
+        $serviceManager->set('test', new \Owl\Router\Router());
         $this->assertTrue($serviceManager->has('test'));
+
+        $this->assertInstanceOf('Owl\Router\Router', $serviceManager->get('test'));
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Instance wasn't found by name: test
+     */
+    public function testExceptionOnUnknownInstance()
+    {
+        $serviceManager = new \Owl\Di\Container();
+        $this->assertFalse($serviceManager->has('test'));
+
+        $serviceManager->get('test');
     }
 }
